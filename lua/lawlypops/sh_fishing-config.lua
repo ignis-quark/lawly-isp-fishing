73e629267d62408592afd600be986737b8e89237
Lawlypops <benloz1998@gmail.com>
2023-08-18 18:53:46 -0500

    Adjustment to config

--- lua/lawlypops/sh_fishing-config.lua
+++ lua/lawlypops/sh_fishing-config.lua
@@ -2,11 +2,11 @@ LAWLYFISH = LAWLYFISH or {}
 
 --Item Configs
 //Multiplier of base item cost to subtract based on length. (at 0.75, MinLength will reduce item price to 25% base value)
-LAWLYFISH.LengthCostMod = 0.75
+LAWLYFISH.LengthCostMod = 0.2
 //Units away a bucket can sell to a bait and tackle shop.
 LAWLYFISH.SellDistance = 200
 //Max items a bucket can hold. BEWARE: NETWORKING LIMITS.
-LAWLYFISH.BucketCapacity = 40
+LAWLYFISH.BucketCapacity = 100
 //Minimum random length for fish to be caught (cm).
 LAWLYFISH.MinLength = 2
 
