b372a41a1baac0339eb542fdfd5423c6c2aca4fa
Lawlypops <benloz1998@gmail.com>
2023-09-07 19:44:19 -0500

    a

--- lua/lawlypops/sv_fishing_networking.lua
+++ lua/lawlypops/sv_fishing_networking.lua
@@ -4,6 +4,7 @@ net.Receive("lawly_fishing_sell_items", function(len, ply)
     local ent = net.ReadEntity()
     local cmd = net.ReadUInt(2)
     if !IsValid(ent) then return end
+    if !ent:CheckForShop(ply) then return end
     if cmd == 0 then --Sell All
         ent:SellAll(ply)
     elseif cmd == 1 then --Sell Trash
