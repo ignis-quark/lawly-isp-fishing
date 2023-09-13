8cc5d963782a1c77248f47421e92b45f54d6bee9
Lawlypops <benloz1998@gmail.com>
2023-09-12 21:05:25 -0500

    Improved Bar Look

--- lua/lawlypops/weapons/lawly_fishingrod/cl_lawly_fishingrod.lua
+++ lua/lawlypops/weapons/lawly_fishingrod/cl_lawly_fishingrod.lua
@@ -8,16 +8,23 @@ SWEP.WhiteColor = Color(255,255,255)
 SWEP.RedColor = Color(255,0,0)
 
 
+local barX, barY = ScrW()*0.4, ScrH()*0.4
+local barW, barH = ScrW()*0.01, ScrH() * 0.2
+
 function SWEP:DrawHUD()
     draw.NoTexture()
     if !self:GetBucket() then
-        draw.SimpleTextOutlined("No bucket with space available. Catch and Release only.", "DermaDefault", ScrW()/2 - 20, ScrH()/2, self.WhiteColor, TEXT_ALIGN_RIGHT, TEXT_ALIGN_CENTER, 1, self.RedColor)
+        draw.SimpleTextOutlined("No bucket with space available. Catch and Release only.", "DermaDefault", ScrW()/2, ScrH()/2 - 20, self.WhiteColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, self.RedColor)
     end
     if self:GetStartChargeTime() != 0 then
         local chargeRatio = math.Clamp((CurTime() - self:GetStartChargeTime()) / self.MaxChargeTime, 0, 1)
-        local StrengthHeight = ScrH() * 0.2 * chargeRatio
-        surface.SetDrawColor(255*chargeRatio, 255 - 255*chargeRatio, 0, 180)
-        surface.DrawRect(ScrW()*0.3, ScrH()*0.6 - StrengthHeight, ScrW()*0.01, StrengthHeight)
+        local StrengthHeight = barH * chargeRatio
+        surface.SetDrawColor(0,0,0,200)
+        surface.DrawOutlinedRect(barX-2, barY-2, barW+4, barH+4, 2)
+        surface.SetDrawColor(39,39,39,200)
+        surface.DrawRect(barX-2, barY-2, barW+4, barH+4)
+        surface.SetDrawColor(255*math.floor(chargeRatio), 255 - 255*math.floor(chargeRatio), 255 - 255*math.floor(chargeRatio), 180)
+        surface.DrawRect(barX, ScrH()*0.6 - StrengthHeight, barW, StrengthHeight)
     end
 
     if !IsValid(self:GetBobber()) then return end
